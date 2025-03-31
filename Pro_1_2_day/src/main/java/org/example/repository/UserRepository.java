497c906f020956ac5d945a9e0e4837b33ccfa145
Green PC <83514730+xacq@users.noreply.github.com>
2025-03-30 23:05:02 -0500

    Delete Pro_1_2_day directory

--- Pro_1_2_day/src/main/java/org/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.User;
-
-public interface UserRepository {
-    User findById(Long id);
-    User save(User user);
-    void delete(Long id);
-}
